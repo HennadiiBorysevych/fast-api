e49e11dd40d90bbfd741eed5e2ac81557609dd20
Hennadii Borysevych <genuch3@gmail.com>
2024-09-29 16:30:40 +0200

    query parameters

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from enum import Enum
-from urllib.request import Request
-
 from fastapi import FastAPI
+from typing_extensions import Optional
 
 app = FastAPI()
 
@@ -53,3 +52,49 @@ def get_food(food_name: FoodEnum):
         return {"food_name": FoodEnum[food_name], "message": "you are health"}
     else:
         return {"food_name": food_name, "message": "you are not healthy"}
+
+
+fake_items_db = [
+    {
+        "item_id": "1",
+    },
+    {
+        "item_id": "2",
+    },
+    {
+        "item_id": "3",
+    }
+]
+
+
+@app.get("/items")
+def items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "This is the first item",
+            }
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+def get_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"user_id": user_id, "item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "This is the second item",
+            }
+        )
+    return item
